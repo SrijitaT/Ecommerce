a58b5186a12c63e06a88cb523dee60be8361ca48
Srijita Thakur <saunak@bitcanny.com>
2019-04-10 10:51:52 +0530

    style changes

--- src/components/pages/itemDetails.js
+++ src/components/pages/itemDetails.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import item_img from "../../../public/images/item_img.jpg";
+import "../../../public/stylesheets/style.css";
+
+
 class ItemDetails extends Component {
     render() {
         const {item}=this.props;
         return (
             <div className="col-sm-4">
-            <div className="card border-default mb-3" style={{maxWidth: '18rem'}}>
-  <div className="card-header bg-transparent border-default">{item.name}</div>
-  <div className="card-body text-primary">
-    <h5 className="card-title">{item.description}</h5>
-    <p className="card-text">Price: Rs {item.price}</p>
-  </div>
-  <div className="card-footer bg-transparent border-default">
-  <a href="#" class="btn btn-warning">Add to cart</a>
-  </div>
-</div>
+              <div className="row custom-card">
+                  <div className="col-sm-6">
+                      <img src={item_img} width="100%" height="90%"/>
+                  </div>
+                  <div className="col-sm-6">
+                          <div>{item.name}</div>
+                          <h5>{item.description}</h5>
+                          <p>Price: Rs {item.price}</p>
+                          <a href="#" class="btn btn-primary">Add to cart</a>
+                      
+                    </div>
+                </div>             
             </div>
         );
     }
